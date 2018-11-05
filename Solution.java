3f07621d39c3c36a29b631c5fbc723035b27d973
VINODKUMARREDDYM <43471357+VINODKUMARREDDYM@users.noreply.github.com>
2018-11-05 18:00:44 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,216 +1,205 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-
-class MinPQ<Key> implements Iterable<Key> {
-    private Key[] pq;                    // store items at indices 1 to n
-    private int n;                       // number of items on priority queue
-    private Comparator<Key> comparator;  // optional comparator
-
-    public MinPQ(int initCapacity) {
-        pq = (Key[]) new Object[initCapacity + 1];
-        n = 0;
-    }
-    public MinPQ() {
-        this(1);
-    }
-    public MinPQ(int initCapacity, Comparator<Key> comparator) {
-        this.comparator = comparator;
-        pq = (Key[]) new Object[initCapacity + 1];
-        n = 0;
-    }
-    public MinPQ(Comparator<Key> comparator) {
-        this(1, comparator);
-    } 
-    public MinPQ(Key[] keys) {
-        n = keys.length;
-        pq = (Key[]) new Object[keys.length + 1];
-        for (int i = 0; i < n; i++)
-            pq[i+1] = keys[i];
-        for (int k = n/2; k >= 1; k--)
-            sink(k);
-        assert isMinHeap();
-    }
-    public boolean isEmpty() {
-        return n == 0;
-    }
-    public int size() {
-        return n;
-    }
-    public Key min() {
-        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
-        return pq[1];
-    }
-    private void resize(int capacity) {
-        assert capacity > n;
-        Key[] temp = (Key[]) new Object[capacity];
-        for (int i = 1; i <= n; i++) {
-            temp[i] = pq[i];
-        }
-        pq = temp;
-    }
-    public void insert(Key x) {
-        // double size of array if necessary
-        if (n == pq.length - 1) resize(2 * pq.length);
-
-        // add x, and percolate it up to maintain heap invariant
-        pq[++n] = x;
-        swim(n);
-        assert isMinHeap();
-    }
-    
-    public Key delMin() {
-        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
-        Key min = pq[1];
-        exch(1, n--);
-        sink(1);
-        pq[n+1] = null;     // to avoid loiterig and help with garbage collection
-        if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
-        assert isMinHeap();
-        return min;
-    }
-    private void swim(int k) {
-        while (k > 1 && greater(k/2, k)) {
-            exch(k, k/2);
-            k = k/2;
-        }
-    }
-
-    private void sink(int k) {
-        while (2*k <= n) {
-            int j = 2*k;
-            if (j < n && greater(j, j+1)) j++;
-            if (!greater(k, j)) break;
-            exch(k, j);
-            k = j;
-        }
-    }
-    private boolean greater(int i, int j) {
-        if (comparator == null) {
-            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
-        }
-        else {
-            return comparator.compare(pq[i], pq[j]) > 0;
-        }
-    }
-
-    private void exch(int i, int j) {
-        Key swap = pq[i];
-        pq[i] = pq[j];
-        pq[j] = swap;
-    }
-
-    // is pq[1..N] a min heap?
-    private boolean isMinHeap() {
-        return isMinHeap(1);
-    }
-
-    // is subtree of pq[1..n] rooted at k a min heap?
-    private boolean isMinHeap(int k) {
-        if (k > n) return true;
-        int left = 2*k;
-        int right = 2*k + 1;
-        if (left  <= n && greater(k, left))  return false;
-        if (right <= n && greater(k, right)) return false;
-        return isMinHeap(left) && isMinHeap(right);
-    }
-    public Iterator<Key> iterator() {
-        return new HeapIterator();
-    }
-
-    private class HeapIterator implements Iterator<Key> {
-         private MinPQ<Key> copy;
-        public HeapIterator() {
-            if (comparator == null) copy = new MinPQ<Key>(size());
-            else                    copy = new MinPQ<Key>(size(), comparator);
-            for (int i = 1; i <= n; i++)
-                copy.insert(pq[i]);
-        }
-
-        public boolean hasNext()  { return !copy.isEmpty();                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Key next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            return copy.delMin();
-        }
-    }
-    
-}
-public class Solution implements Comparable<Solution>{
-	private final int sum;
-	private final int i;
-	private final int j;
-	
-	public Solution(int i,int j)
-	{
-		this.sum=i*i*i+j*j*j;
-		this.i=i;
-		this.j=j;
-	}
+class Book
+{
+	String Name;
+	String Author;
+	double Price;
+	Book(String Name,String Author,double d)
+		{
+		this.Name=Name;
+		this.Author=Author;
+		this.Price=d;
+		}
 	@Override
-	public int compareTo(Solution that) {
-		// TODO Auto-generated method stub
-		if(this.sum<that.sum)
-			return -1;
-		if(this.sum>that.sum)
-			return 1;
+	public String toString() {
+		return Name+","+" "+Author+","+" "+Price;
+	}
+	
+}
+class Node
+{
+	public Book key;
+	public int value;
+	Node left;
+	public Node right;
+	public Node(Book key,int value)
+	{
+		this.key=key;
+		this.value=value;
+	}
+}
+public class Solution {
+	Node root;
+	public void put(Book key,int value)
+	{
+		root=put(root,key,value);
+		/*Node temp=root;
+		disp(temp);*/
+	}
+	public Node put(Node node,Book Key,int value)
+	{
+		if(node==null)
+			return new Node(Key,value);
+		if(Key.Name.compareTo(node.key.Name)<0)
+			node.left=put(node.left,Key,value);
+		if(Key.Name.compareTo(node.key.Name)>0)
+			node.right=put(node.right,Key,value);
+		if(Key.Name.compareTo(node.key.Name)==0)
+			node.value=value;
+		//root.
+		return node;
+		
+	}
+	public int get(Book Key)
+	{
+		Node node=root;
+		while(node!=null)
+		{
+			if(Key.Name.compareTo(node.key.Name)<0)
+				node=node.left;
+			else if(Key.Name.compareTo(node.key.Name)>0)
+				node=node.right;
+			else
+				return node.value;
+			
+		}
 		return 0;
 	}
-	public int getSum()
-    {
-    	return sum;
-    }
-	public String toString()
+	public Book min()
 	{
-		String s=""+sum;
 		
-		return s;//+"="+i+"^3"+"+"+j+"3";
+	    return min(root).key;
+	}
+	private Node min(Node current) { 
+	    if (current.left == null) return current; 
+	    else                      return min(current.left); 
+	}
+	public Book max() {
+	    
+	    return max(root).key;
+	} 
+	
+	private Node max(Node current) {
+	    if (current.right == null) return current; 
+	    else                       return max(current.right); 
+	}
+	public  Book floor(Book key)
+	{
+		Node x = floor(root, key);
+	    if (x == null) return null;   // if empty, return null
+	    else           return x.key;
+	}
+	private Node floor(Node current, Book key) {
+	    if (current == null) return null;
+	    if(key.Name.compareTo(current.key.Name)==0)
+	    	return current;
+	    if(key.Name.compareTo(current.key.Name)<0)
+	    	return floor(current.left,key);
+	    
+	    
+	    Node t = floor(current.right, key); 
+	    if (t != null) return t;
+	    else           return current;
+	}
+	public Book ceiling(Book key) {
+	  
+	    Node x = ceiling(root, key);
+	    if (x == null) return null;   // if empty, return null
+	    else return x.key;
+	}
+	
+	private Node ceiling(Node current, Book key) {
+	    if (current == null) return null;
+	    if(key.Name.compareTo(current.key.Name)==0)
+	    {
+	    	//System.out.println("*****");
+	    	return current;}
+	    if(key.Name.compareTo(current.key.Name)<0)
+	    {
+	    	Node t = ceiling(current.left, key); 
+			    if (t != null) return t;
+			    else           return current;
+	    }
+	    return ceiling(current.right,key);
+	}
+	void select(int k)
+	{
+	ArrayList<Book> al=new ArrayList<Book>();
+		select(root,al);
+		for(int i=0;i<al.size()&&i<=k;i++)
+			if(i==k)
+				System.out.println(al.get(i));
+		//System.out.println(j);
+		
+	}
+	
+	
+	void select(Node a,ArrayList al)
+	{
+		if(a!=null)
+		{
+			select(a.left,al);
+			al.add(a.key);
+			select(a.right,al);
+		}
+		
+		
 	}
 	public static void main(String args[])
 	{
-		int n = 900;
-        Scanner sc=new Scanner(System.in);
-        while(sc.hasNext())
-        {
-        	String sa=sc.nextLine();
-        	String[] b=sa.split(" ");
-        
-        int o=Integer.parseInt(b[0]);
-        int m=Integer.parseInt(b[1]);
-        int count=0;
-        int temp=3;
-        // initialize priority queue
-        MinPQ<Solution> pq = new MinPQ<Solution>();
-        for (int i = 0; i <= n; i++) {
-        	//System.out.println();
-            pq.insert(new Solution(i, i));
-        }
+		Scanner scan = new Scanner(System.in);
+		Solution or=new Solution();
+		/*Book book=new Book("Algorithms","Bob Sedgewick",6000.0);
+		Book book1=new Book("Python","eric",5000);
+		Book book2=new Book("Hello","Ajay",200.0);
+		Book book3=new Book("IT","Viswas",400.0);
+		or.put(book, 1);
+		or.put(book1, 2);
+		System.out.println(or.get(book));
+		or.put(book2, 5);
+		or.put(book3,2);
+		System.out.println(or.max());
+		System.out.println(or.get(book3));
+		System.out.println(or.min());
+		or.select(1);
+		System.out.println(or.floor(book));
+		System.out.println(or.ceiling(book1));*/
+		
+		
+				
+		while(scan.hasNext())
+		{
+			String s = scan.nextLine();
+			String [] array = s.split(",");
+			String Operation = array[0];
+			switch (Operation)
+			{
+			case "put" :Book book = new Book(array[1],array[2],Double.parseDouble(array[3]));
+							or.put(book,Integer.parseInt(array[4]));
+						break;
+			case "get" :Book book1 = new Book(array[1],array[2],Double.parseDouble(array[3]));
+							if(or.get(book1)!=0)
+							System.out.println(or.get(book1));
+						else
+							System.out.println("null");
+						break;
+			case "min":System.out.println(or.min());
+						break;
+			case "max":System.out.println(or.max());
+						break;
+			case "floor":Book book11 = new Book(array[1],array[2],Double.parseDouble(array[3]));
+							System.out.println(or.floor(book11));
+						break;
+			case "ceiling":Book book111 = new Book(array[1],array[2],Double.parseDouble(array[3]));
+			System.out.println(or.ceiling(book111));
+						break;
+			case "select":or.select(Integer.parseInt(array[1]));
+						break;
+			}
+		}
+	}
 
-        // find smallest sum, print it out, and update
-        while (!pq.isEmpty()) {
-        	Solution s = pq.delMin();
-            if (temp == s.getSum()) {
-                count++;
-            } else {
-                count = 0;
-            }
-            if (count == m - 1) {
-                o--;
-                if (o == 0) {
-                    System.out.println(s.getSum());
-                    break;
-                }
-            }
-            temp = s.getSum();
-            if (s.j < n) {
-                pq.insert(new Solution(s.i, s.j + 1));
-            }
-    }
-    }
-    }
 }
