251abf085e617b9233dfa579fdd6e475c600631d
VINODKUMARREDDYM <43471357+VINODKUMARREDDYM@users.noreply.github.com>
2018-11-05 09:10:46 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,104 +1,216 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-class Book
-{
-	String Name;
-	String Author;
-	float Price;
-	Book(String Name,String Author,float Price)
-		{
-		this.Name=Name;
-		this.Author=Author;
-		this.Price=Price;
-		}
-	@Override
-	public String toString() {
-		return "Book [Name=" + Name + ", Author=" + Author + ", Price=" + Price + "]";
-	}
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+class MinPQ<Key> implements Iterable<Key> {
+    private Key[] pq;                    // store items at indices 1 to n
+    private int n;                       // number of items on priority queue
+    private Comparator<Key> comparator;  // optional comparator
+
+    public MinPQ(int initCapacity) {
+        pq = (Key[]) new Object[initCapacity + 1];
+        n = 0;
+    }
+    public MinPQ() {
+        this(1);
+    }
+    public MinPQ(int initCapacity, Comparator<Key> comparator) {
+        this.comparator = comparator;
+        pq = (Key[]) new Object[initCapacity + 1];
+        n = 0;
+    }
+    public MinPQ(Comparator<Key> comparator) {
+        this(1, comparator);
+    } 
+    public MinPQ(Key[] keys) {
+        n = keys.length;
+        pq = (Key[]) new Object[keys.length + 1];
+        for (int i = 0; i < n; i++)
+            pq[i+1] = keys[i];
+        for (int k = n/2; k >= 1; k--)
+            sink(k);
+        assert isMinHeap();
+    }
+    public boolean isEmpty() {
+        return n == 0;
+    }
+    public int size() {
+        return n;
+    }
+    public Key min() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        return pq[1];
+    }
+    private void resize(int capacity) {
+        assert capacity > n;
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 1; i <= n; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
+    }
+    public void insert(Key x) {
+        // double size of array if necessary
+        if (n == pq.length - 1) resize(2 * pq.length);
+
+        // add x, and percolate it up to maintain heap invariant
+        pq[++n] = x;
+        swim(n);
+        assert isMinHeap();
+    }
+    
+    public Key delMin() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        Key min = pq[1];
+        exch(1, n--);
+        sink(1);
+        pq[n+1] = null;     // to avoid loiterig and help with garbage collection
+        if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
+        assert isMinHeap();
+        return min;
+    }
+    private void swim(int k) {
+        while (k > 1 && greater(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2*k <= n) {
+            int j = 2*k;
+            if (j < n && greater(j, j+1)) j++;
+            if (!greater(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+    private boolean greater(int i, int j) {
+        if (comparator == null) {
+            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
+        }
+        else {
+            return comparator.compare(pq[i], pq[j]) > 0;
+        }
+    }
+
+    private void exch(int i, int j) {
+        Key swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+    }
+
+    // is pq[1..N] a min heap?
+    private boolean isMinHeap() {
+        return isMinHeap(1);
+    }
+
+    // is subtree of pq[1..n] rooted at k a min heap?
+    private boolean isMinHeap(int k) {
+        if (k > n) return true;
+        int left = 2*k;
+        int right = 2*k + 1;
+        if (left  <= n && greater(k, left))  return false;
+        if (right <= n && greater(k, right)) return false;
+        return isMinHeap(left) && isMinHeap(right);
+    }
+    public Iterator<Key> iterator() {
+        return new HeapIterator();
+    }
+
+    private class HeapIterator implements Iterator<Key> {
+         private MinPQ<Key> copy;
+        public HeapIterator() {
+            if (comparator == null) copy = new MinPQ<Key>(size());
+            else                    copy = new MinPQ<Key>(size(), comparator);
+            for (int i = 1; i <= n; i++)
+                copy.insert(pq[i]);
+        }
+
+        public boolean hasNext()  { return !copy.isEmpty();                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Key next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return copy.delMin();
+        }
+    }
+    
+}
+public class Solution implements Comparable<Solution>{
+	private final int sum;
+	private final int i;
+	private final int j;
 	
-}
-class Node
-{
-	public Book key;
-	public int value;
-	public Node left,right;
-	public Node(Book key,int value)
+	public Solution(int i,int j)
 	{
-		this.key=key;
-		this.value=value;
+		this.sum=i*i*i+j*j*j;
+		this.i=i;
+		this.j=j;
 	}
-}
-class BinarySearch
-{
-	Node root;
-	public void put(Book key,int value)
-	{
-		root=put(root,key,value);
-		/*Node temp=root;
-		disp(temp);*/
-	}
-	public Node put(Node node,Book Key,int value)
-	{
-		if(node==null)
-			return new Node(Key,value);
-		if(Key.Name.compareTo(node.key.Name)<0)
-			node.left=put(node.left,Key,value);
-		if(Key.Name.compareTo(node.key.Name)>0)
-			node.right=put(node.right,Key,value);
-		if(Key.Name.compareTo(node.key.Name)==0)
-			node.value=value;
-		return node;
-		
-	}
-	public int get(Book Key)
-	{
-		Node node=root;
-		while(node!=null)
-		{
-			if(Key.Name.compareTo(node.key.Name)<0)
-				node=node.left;
-			else if(Key.Name.compareTo(node.key.Name)>0)
-				node=node.right;
-			else
-				return node.value;
-			
-		}
+	@Override
+	public int compareTo(Solution that) {
+		// TODO Auto-generated method stub
+		if(this.sum<that.sum)
+			return -1;
+		if(this.sum>that.sum)
+			return 1;
 		return 0;
 	}
-	public void disp(Node temp)
-	{  if(temp!=null) {
-		disp(temp.left);
-		System.out.println(temp.key+""+temp.value);	
-		disp(temp.right);
-	}
-	}
-}
-public class Solution {
-public static void main(String args[])
-{
-	Scanner scan = new Scanner(System.in);
-	BinarySearch bst = new BinarySearch();
-	while(scan.hasNext())
+	public int getSum()
+    {
+    	return sum;
+    }
+	public String toString()
 	{
-		String s = scan.nextLine();
-		String [] array = s.split(",");
-		String Operation = array[0];
-		Book book = new Book(array[1],array[2],Float.parseFloat(array[3]));
-		switch (Operation)
-		{
-		case "put" :
-			bst.put(book,Integer.parseInt(array[4]));
-			
-			break;
-		case "get" :
-			if(bst.get(book)!=0)
-				System.out.println(bst.get(book));
-			else
-				System.out.println("null");
-			break;
-		}
+		String s=""+sum;
+		
+		return s;//+"="+i+"^3"+"+"+j+"3";
 	}
+	public static void main(String args[])
+	{
+		int n = 900;
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext())
+        {
+        	String sa=sc.nextLine();
+        	String[] b=sa.split(" ");
+        
+        int o=Integer.parseInt(b[0]);
+        int m=Integer.parseInt(b[1]);
+        int count=0;
+        int temp=3;
+        // initialize priority queue
+        MinPQ<Solution> pq = new MinPQ<Solution>();
+        for (int i = 0; i <= n; i++) {
+        	//System.out.println();
+            pq.insert(new Solution(i, i));
+        }
 
-	
-}
+        // find smallest sum, print it out, and update
+        while (!pq.isEmpty()) {
+        	Solution s = pq.delMin();
+            if (temp == s.getSum()) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count == m - 1) {
+                o--;
+                if (o == 0) {
+                    System.out.println(s.getSum());
+                    break;
+                }
+            }
+            temp = s.getSum();
+            if (s.j < n) {
+                pq.insert(new Solution(s.i, s.j + 1));
+            }
+    }
+    }
+    }
 }
